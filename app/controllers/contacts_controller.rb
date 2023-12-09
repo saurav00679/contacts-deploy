36a7ed8eb8649c860ffb0c8bc88a349cf7ef89b5
Saurav Sharma <sauravsharma.00679@gmail.com>
2023-12-09 12:14:26 +0530

    ilike statement

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,11 +1,14 @@
 class ContactsController < ApplicationController
   def index
+    contacts = Contact.all
+    render json: contacts.all
   end
 
   def search
     value = params["searchKey"]
 
-    contacts = Contact.where('contact_number like ? or name like ?', "#{value}%", "#{value}%")
+    contacts = Contact.where('contact_number ilike ? OR name ilike ?', "%#{value}%", "%#{value}%")
+
     render json: contacts
   end
 end
